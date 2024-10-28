aac3af05a186a37a326260143d8517ed035b085e
zouxin <zlin368@connect.hkust-gz.edu.cn>
2024-10-28 12:29:09 +0800

    练习

---
+++ C++practice/not_late.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int s,v;
+    cin>>s>>v;
+    int t = s/v;
+    if(s%v!=0) t++;
+    t+=10;
+    int hour=t/60;
+    if(hour>8)
+    {hour=24-(hour-7);}
+    else hour=7-hour;
+    int min=60-t%60;
+    if(hour<10&&min<10) cout<<"0"<<hour<<":"<<"0"<<min;
+    else if(hour<10) cout<<"0"<<hour<<":"<<min;
+    else if(min<10) cout<<hour<<":"<<"0"<<min;
+    else cout<<hour<<":"<<min;
+    return 0;
+}
